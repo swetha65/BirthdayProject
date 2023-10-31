d84d403e403762dcda158ae0db29083aa801751a
swetha65 <67499935+swetha65@users.noreply.github.com>
2023-10-31 19:26:05 +0530

    Change

--- scripts/BirthdayChecker.py
+++ scripts/BirthdayChecker.py
@@ -3,6 +3,39 @@ import datetime
 import pandas as pd
 import os
 
+template = '''
+Wishing you a very Happy Birthday {{name}}! May you be gifted with life's biggest joys and never-ending bliss.
+
+Regards,
+CP Team
+'''
+
+def fillTemplate():
+	birthdayList = checkBirthday()
+	if birthdayList[0] == 0:
+		return ""
+	elif birthdayList[0] == 1:
+		name = birthdayList[1][0]
+	else:
+		name = ""
+		for i in range(0,len(birthdayList[1])):
+			if i == (len(birthdayList[1]) - 2):
+				name += birthdayList[1][i] + " "
+			elif i == (len(birthdayList[1]) - 1):
+				name += "and " + birthdayList[1][i]
+			else:
+				name += birthdayList[1][i] + ", "
+	message = f'''
+Wishing you a very Happy Birthday {name}! May you be gifted with life's biggest joys and never-ending bliss.
+
+Regards,
+CP Team
+'''	
+	print(message)
+
+
+        
+
 def checkBirthday():
 	cur_path = os.path.dirname(__file__)
 	pathlist = os.path.split(cur_path)
@@ -19,7 +52,7 @@ def checkBirthday():
 			numOfBirthdays += 1
 			birthdayList.append(name)
 
-	return([numOfBirthdays,birthdayList])
+	return [numOfBirthdays, birthdayList]
 
-print(checkBirthday())
+print(fillTemplate())
 
